8464297691f6849a54cec52bdc037b550bf42b1e
Travis Falls <369837+N3rd007@users.noreply.github.com>
2023-10-17 21:34:53 -0400

    initial changes

--- packages/blockstore-azure/src/index.ts
+++ packages/blockstore-azure/src/index.ts
@@ -3,6 +3,7 @@ import * as Errors from 'blockstore-core/errors'
 import { NextToLast, type ShardingStrategy } from "./sharding.js"
 import type { AbortOptions } from "interface-store"
 import { BlockBlobClient } from "@azure/storage-blob"
+import type { Pair } from "interface-blockstore"
 import type { CID } from 'multiformats/cid'
 
 export interface AzureBlockstoreInit {
@@ -60,21 +61,60 @@ export class AzureBlockstore extends BaseBlockstore {
      */
     async get (key: CID, options?: AbortOptions): Promise<Uint8Array> {
         try {
-            // 
+            // getting blob name from the azure client
+            const blobName = await this.azureClient.getBlobName(key, { abortSignal: options?.signal });
+
+            // getting blob client from the azure client
+            const blobClient = await this.azureClient.getBlockBlobClient(blobName, { abortSignal: options?.signal });
+
+            // downloading the blob
+            const response = await blobClient.download(0, undefined, { abortSignal: options?.signal });
+
+            // reading the stream as uint8 array and returning it
+            return this.readStreamAsUint8Array(response.readableStreamBody);
         } catch (err: any) {
             if (err.statusCode === 404) {
-                throw Errors.notFoundError(err)
+                throw Errors.notFoundError(err);
             }
-            throw err
+            throw err;
         }
     }
+
+    /** 
+     * Private method for reading the stream into a uint array
+    **/
+    private async readStreamAsUint8Array(stream: NodeJS.ReadableStream): Promise<Uint8Array> {
+        const chunks: Uint8Array[] = [];
+        for await (const chunk of stream) {
+            if (typeof chunk === 'string' || chunk instanceof Buffer) {
+                // Convert string or Buffer chunks to Uint8Array
+                chunks.push(new Uint8Array(Buffer.from(chunk)));
+            } else {
+                // Assume it's already Uint8Array
+                chunks.push(chunk);
+            }
+        }
+        return Buffer.concat(chunks);
+    }
+
 
     /**
      * Check for the existence of the given key
      */
     async has (key: CID, options?: AbortOptions): Promise<boolean> {
         try {
-            // add azure implementation
+            // Getting blob name from the azure client
+            const blobName = await this.azureClient.getBlobName(key, { abortSignal: options?.signal });
+
+            // Getting blob client from the azure client
+            const blobClient = this.azureClient.getBlockBlobClient(blobName, { abortSignal: options?.signal });
+
+            // Check if the blob exists
+            const blobProperties = await blobClient.getProperties({ abortSignal: options?.signal });
+
+            // If the blob exists, return true; otherwise, return false;
+            return blobProperties === null ? false : true;
+            
         } catch (err: any) {
             // doesn't exist and permission policy includes list container
             if (err.$metadata?.httpStatusCode === 404) {
@@ -95,18 +135,40 @@ export class AzureBlockstore extends BaseBlockstore {
      */
     async delete (key: CID, options?: AbortOptions): Promise<void> {
         try {
-            // add azure implementation
+            // Getting blob name from the azure client
+            const blobName = await this.azureClient.getBlobName(key, { abortSignal: options?.signal });
+
+            // Getting blob client from the azure client
+            const blobClient = this.azureClient.getBlockBlobClient(blobName, { abortSignal: options?.signal });
+
+            // Delete the blob
+            await blobClient.delete({ abortSignal: options?.signal });
+
         } catch (err: any) {
             throw Errors.deleteFailedError(err)
         }
     }
 
     async * getAll (options?: AbortOptions): AsyncIterable<Pair> {
-        const params: Record<string, any> = {}
+        // TODO: ask about this params
+        //const params: Record<string, any> = {}
 
         try {
-            while (true) {
-                // add azure implementation
+            // Getting container client from the azure client
+            const containerClient = this.azureClient.getContainerClient(this.container);
+
+            // Use the `listBlobsFlat` method to list all blobs in the container
+            const blobItems = containerClient.listBlobsFlat({ abortSignal: options?.signal });
+
+            // Iterate over the blobs and print the name and length of each
+            for await (const blobItem of blobItems) {
+                // Convert blobItem to a Pair or your desired data structure
+                const pair: Pair = {
+                    key: blobItem.name, // Set the key to the blob name
+                    value: blobItem.properties.contentLength // Set the value to the content length or other relevant information
+                };
+
+                yield pair;
             }
         } catch (err: any) {
             throw new Error(err.code)
@@ -118,13 +180,30 @@ export class AzureBlockstore extends BaseBlockstore {
      */
     async open (options?: AbortOptions): Promise<void> {
         try {
-            // add azure implementation
+            // Getting container client from the azure client
+            const containerClient = this.azureClient.getContainerClient(this.container);
+
+            // Use the `getProperties` method to check if the container exists
+            await containerClient.getProperties({ abortSignal: options?.signal });
+
         } catch (err: any) {
             if (err.statusCode !== 404) {
                 if (this.createIfMissing) {
-                    // add azure implementation
+                    // Optionally, create the container if it doesn't exist
+                    await this.azureClient.getContainerClient(this.container).create({ abortSignal: options?.signal });
                 }
-                throw Errors.openFailedError(err)
+                else {
+                    // If not set to create, throw an error
+                    throw Errors.openFailedError(err);
+                }
+            }
+            else {
+                // The container does not exist, and createIfMissing is true
+                if (this.createIfMissing) {
+                    await this.azureClient.getContainerClient(this.container).create({ abortSignal: options?.signal });
+                } else {
+                    throw Errors.openFailedError(err);
+                }
             }
         }
     }
